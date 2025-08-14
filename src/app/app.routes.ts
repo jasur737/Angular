525a1dd33404b2e0348925b106a0cd704622f40e
jasur737 <baxtiyorovjasur728@gmail.com>
2025-08-14 16:41:48 +0500

    ;)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './Pages/home/home';
+import { About } from './Pages/about/about';
+import { NotFound } from './Pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: Home},
+    {path: 'about', component: About},
+    {path: 'not-found', component: NotFound},
+    {path: '**', redirectTo: 'not-found'},
+];
